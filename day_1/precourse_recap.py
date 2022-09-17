81502fd02ac5b1546611a7eb4d2bbf267ba3f26e
Georgi <ggeorgeuk@gmail.com>
2022-09-17 08:40:32 +0100

    Create precourse_recap.py

---
+++ day_1/precourse_recap.py
@@ -0,0 +1,11 @@
+str_ = "string"
+int_ = "integer"
+float_ = "float"
+list_ = "list"
+dict_ = "dictionaty"
+tuple_ = "tuple"
+num_1 = 10
+num_2 = 5
+
+
+print(f"Immutable data types in Python:\n {str_} {int_} {float_}\nMutable data types in Python:\n {list_} {dict_} {tuple_}\nMethods example:\n Invoke upper method on str_ go in {str_.upper()}\nOperators example:\n Modulus operator on num_1 and num2 go in {num_1 % num_2}")
