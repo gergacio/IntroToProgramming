4fe5f24ba4269fa39862f2085369f5e7310cc133
Georgi <ggeorgeuk@gmail.com>
2022-09-19 12:03:08 +0100

    Add files via upload

--- weekend/pet_shop.py
+++ weekend/pet_shop.py
@@ -1,67 +1,18 @@
 # WRITE YOUR FUNCTIONS HERE
+from ast import Num
+
+
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
+
 def get_total_cash(pet_shop):
-    return pet_shop["admin"]["total_cash"]   
-def add_or_remove_cash(pet_shop, amount):
-    pet_shop["admin"]["total_cash"] += amount
-    
-def get_pets_sold(pet_shop):
-    return pet_shop["admin"]["pets_sold"]
-def increase_pets_sold(pet_shop,amount):
-    pet_shop["admin"]["pets_sold"] += amount
-def get_stock_count(pet_shop):
-    return len(pet_shop["pets"])
-def get_pets_by_breed(pet_shop, breed):
-    pets_by_breed = []
-    for pet in pet_shop["pets"]:
-        if pet["breed"] == breed:
-            pets_by_breed.append(pet)
-    return pets_by_breed      
-def find_pet_by_name(pet_shop, name):
-    for pet in pet_shop["pets"]:
-        if pet["name"] == name:
-            return pet
-def remove_pet_by_name(pet_shop, name):
-    for pet in pet_shop["pets"]:
-        if pet["name"] == name:
-            pet_shop["pets"].remove(pet) 
+    return pet_shop["admin"]["total_cash"]
 
-            
-def add_pet_to_stock(pet_shop, new_pet):
-    pet_shop["pets"].append(new_pet)   
-def get_customer_cash(customer):
-    return customer["cash"]
-def remove_customer_cash(customer, amount):
-    customer["cash"] -= amount 
-def get_customer_pet_count(customer):
-    return len(customer["pets"])
-def add_pet_to_customer(customer, new_pet):
-    customer["pets"].append(new_pet)
-     
-def customer_can_afford_pet(customer, new_pet):
-    can_afford = False
-    if customer["cash"] >= new_pet["price"]:
-        can_afford = True
-    return can_afford
-def sell_pet_to_customer(pet_shop, pet_for_sale, customer):
-               
-        for pet in pet_shop["pets"]:  
-            # pet in shop + cash = have it  :)
-            if pet["name"] == pet_for_sale["name"]:
-                    if customer["cash"] >= pet["price"]:
-                       #update customer cash
-                       customer["cash"] -= pet["price"]
-                       #push new pet to costemer list
-                       customer["pets"].append(pet)
-                       #update sold pets
-                       pet_shop["admin"]["pets_sold"] += 1
-                       #update pet_shop total cash
-                       pet_shop["admin"]["total_cash"] += pet["price"]
-
-  
-
-  
+def add_or_remove_cash(pet_shop,cash):
+    total = pet_shop["admin"]["total_cash"]
+    if cash >= 0:
+        total += cash 
+    return get_total_cash(pet_shop)       
 
 
 
